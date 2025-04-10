3d58fa97821416bf540372a91eb43f1e95ac3db0
Laurel YAMA <ngwambilaj@gmail.com>
2025-04-10 23:23:12 +0100

    first commit

---
+++ src/main/java/com/esiitech/publication_memoire/PublicationMemoireApplication.java
@@ -0,0 +1,13 @@
+package com.esiitech.publication_memoire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicationMemoireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicationMemoireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esiitech/publication_memoire/PublicationMemoireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esiitech.publication_memoire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicationMemoireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
